1eb02387ccb1b106e5cced068e2edbe9ffe3ba3c
Žan Pevec <pevecyan@users.noreply.github.com>
2018-07-26 00:50:14 +0200

    Update KeyboardZeroV1.0.ino

--- Firmware/KeyboardZeroV1.0/KeyboardZeroV1.0.ino
+++ Firmware/KeyboardZeroV1.0/KeyboardZeroV1.0.ino
@@ -8,6 +8,12 @@ const byte height = 4;
 bool ackMode = false;
 bool customProfile = false;
 
+bool locked = true;
+bool previousPinCorrect = false;
+char currentPinChar = "";
+
+int lockPressCount = 0;
+
 char hexaKeys[height][width] = {
   {'0','1','2','3','4','5'},
   {'6','7','8','9','a','b'},
@@ -21,13 +27,18 @@ byte colPins[width] = {9,8,7,6,5,4};
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, height, width); 
 
 void setup() {
+   customKeypad.addStatedEventListener(keypadEvent); 
   // put your setup code here, to run once:
   Serial.begin(9600);
   Keyboard.begin();
+
+ 
   
 }
 
 void loop() {
+  customKeypad.getKeys();
+  return;
   char customKey = customKeypad.getKey();
   
   if(Serial.available() > 0){
@@ -78,3 +89,127 @@ void handleAcknowledge(){
   Serial.print(name+":"+width+":"+height);
 }
 
+// Taking care of some special events.
+void keypadEvent(KeypadEvent key, KeyState kpadState ){
+    switch (kpadState){
+    case PRESSED:
+        if (locked) checkPin(key, kpadState);
+        else keyPressed(key);
+        break;
+
+    case RELEASED:
+        
+        break;
+
+    case HOLD:
+        
+        break;
+    }
+}
+
+void checkPin(KeypadEvent key, KeyState kpadState ){
+  bool unlocked = false;
+    //PIN 3245 - lkmn
+    if (!previousPinCorrect){
+      if (key == 'l'){
+        previousPinCorrect = true;
+        currentPinChar = 'l';
+      } 
+    }
+    else if (currentPinChar == 'l' && key == 'k'){
+      currentPinChar = 'k';
+    }
+    else if (currentPinChar == 'k' && key == 'm'){
+      currentPinChar = 'm';
+    }
+    else if (currentPinChar == 'm' && key == 'n'){
+      currentPinChar = 'n';
+    }
+    else if (currentPinChar == 'n' && key == 'c'){
+      currentPinChar = "";
+      previousPinCorrect = false;
+      locked = false;
+      printText("Kezboard unlocked\n");
+      unlocked = true;
+      lockPressCount = 0;
+    }
+    else {
+      currentPinChar = "";
+      previousPinCorrect = false;
+      
+    }
+    if (!previousPinCorrect && key == 'c' && !unlocked){
+      printText("Kezboard locked, please enter pin and press SKULL!\n");
+    }
+    
+}
+
+void keyPressed(KeypadEvent key){
+  //First Row
+  if (key == '0'){
+
+  }
+  else if (key == 'c'){
+    lockPressCount++;
+    if (lockPressCount == 4){
+      locked = true;
+      printText("Kezboard LOCKED\n");
+    }
+  }
+  else if (key == 'g'){
+    Keyboard.print(" ");
+    delay(500);
+    Keyboard.print("\n");
+  }
+  else if (key == 'd'){
+    Keyboard.print("\n");
+  }
+  else if (key == 'e'){
+    Keyboard.print("deploy\n");
+  }
+  //4. row
+  else if (key == 'i'){
+    moveToDesktop('1');
+  }
+  else if (key == 'j'){
+    moveToDesktop('2');
+  }
+  else if (key == 'k'){
+    moveToDesktop('3');
+  }
+  else if (key == 'l'){
+    moveToDesktop('4');
+  }
+  else if (key == 'm'){
+    moveToDesktop('5');
+  }
+  else if (key == 'n'){
+    moveToDesktop('6');
+  }
+  
+}
+
+void moveToDesktop(char desktop){
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(desktop);
+
+    Keyboard.release(KEY_LEFT_CTRL);
+    Keyboard.release(KEY_LEFT_ALT);
+    Keyboard.release(desktop);
+}
+
+void backSpace(int count, int delayTime){
+  delay(delayTime);
+  for(int i = 0; i < count; i++){
+    Keyboard.press(8);
+    Keyboard.release(8);
+
+  }
+}
+
+
+void printText(char* text){
+  Keyboard.print(text);
+  backSpace(strlen(text), 1000);
+}
